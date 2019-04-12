1574a0bdb86b18030f498fa6f13585c3091d1078
roy renno <royrenno@yahoo.com>
2019-04-12 05:23:33 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,14 +12,14 @@ class Artist
   end
   def add_song(song)
     @songs <<song
-  end 
-  
+  end
+
   def self.find_or_create_by_name(name)
     self.find(name) ?self.find(name) :self.create(name)
   end
 
   def self.find(name)
-    self.all.find {|artist.name == name} 
+    self.all.find {|artist.name == name}
   end
-  
+
   end

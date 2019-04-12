696df9407e084412295bcf5a026994bf40118339
roy renno <royrenno@yahoo.com>
2019-04-12 05:23:29 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,25 @@
 class Artist
+  @@all =[]
   attr_accessor :name, :song
 
   def initialize(name)
     @name = name
     @song = []
+  end
+
+  def self.all
+    @@all
+  end
+  def add_song(song)
+    @songs <<song
+  end 
+  
+  def self.find_or_create_by_name(name)
+    self.find(name) ?self.find(name) :self.create(name)
+  end
+
+  def self.find(name)
+    self.all.find {|artist.name == name} 
   end
   
   end

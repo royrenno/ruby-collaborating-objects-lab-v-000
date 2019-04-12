8ba7b43a3716b64f3d4ac5c300da1ebed4616a98
roy renno <royrenno@yahoo.com>
2019-04-12 05:37:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,7 @@ class Song
   def self.new_by_filename(filename)
     artist, song = filename.split(" - ")
     new_song = self.new(song)
-    new_song.artist_name = artist_name
+    new_song.artist_name = artist
     new_song
 end
 
